471c4bf257b199d48a2258d167ea730439685794
Tharun Dubbaka <rajithadubbaka4005@gmail.com>
2026-08-13 11:27:05 +0530

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        hmap1={}
+        hmap2={}
+        for i in s:
+            hmap1[i]=hmap1.get(i,0)+1
+        for j in t:
+            hmap2[j]=hmap2.get(j,0)+1
+        return hmap1==hmap2
+
+
